1abe362e5c1446dbaab0e3cd35b812d0b14513f2
Venom <test@neto.com>
2025-09-18 23:06:30 +0300

    Custom Views

---
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -0,0 +1,42 @@
+package ru.netology.statsview
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ru.netology.statsview.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        applyInset(binding.root)
+
+        binding.stats.data = listOf(
+            500F,
+            500F,
+            500F,
+        )
+    }
+
+    private fun applyInset(main: View) {
+        ViewCompat.setOnApplyWindowInsetsListener(main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val imeInsets = insets.getInsets(WindowInsetsCompat.Type.ime())
+            val isImeVisible = insets.isVisible(WindowInsetsCompat.Type.ime())
+            v.setPadding(
+                v.paddingLeft,
+                systemBars.top,
+                v.paddingRight,
+                if (isImeVisible) imeInsets.bottom else systemBars.bottom
+            )
+            insets
+        }
+    }
+}
